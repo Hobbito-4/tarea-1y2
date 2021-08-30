8b897c81e2572af03a8bb3543e993f3c358e51f7
hobbito <cristhianlluncor@gmail.com>
2021-08-30 16:29:47 -0500

    commits

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-let a = +prompt("¿Primer número?", 1);
-let b = +prompt("¿Segundo número?", 2);
+//let a = +prompt("¿Primer número?", 1);
+//let b = +prompt("¿Segundo número?", 2);
 
-alert(a / (b^2));
+//console.log(a / (b^2));
+//tarea 2
+let { name, email } = { name: "John", email: "john@example.com" }
+console.log(name) // "John" console.log(email) // "john@example.com" 
+
+let { nombre,correoelectrónico, resto} = { nombre: "John",correoelectrónico: "john@example.com", city: "Phoenix", state: "AZ",
+country: "USA"};
+resto === { city: "Phoenix", state: "AZ", country: "USA"}; // true
+const user = { nombre: "John",correoelectrónico: "john@example.com",city: "Phoenix", state: "AZ", country: "USA"};
+const str = ({ nombre,correoelectrónico, resto} = user );  
+console.log(user)
